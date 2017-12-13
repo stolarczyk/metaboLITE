b88af9863204d0dfae9af936601c31ffeb10ab32
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2017-12-13 15:56:54 -0500

    Untracked old files

--- change_bounds_wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cwd=$(pwd)
-cd ~/PycharmProjects/uva/
-source bin/activate
-python -W ignore change_bounds.py $1 $2 $3 $4
-mv flux_bounds.txt $cwd/data
-deactivate
-
--- gimme_wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cwd=$(pwd)
-cd ~/PycharmProjects/uva_oldcobra/
-source bin/activate
-python -W ignore runGIMME.py $1
-mv fluxes_gimme.csv $cwd/data
-deactivate
-
