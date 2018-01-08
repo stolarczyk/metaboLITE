ee6c74e53bf209efc55d4a1b965fe599fb32d70f
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-01-08 10:20:57 -0500

    Added absolute path and its converstion to the OS accordingly

--- scripts/prepare_media2.py
+++ scripts/prepare_media2.py
@@ -1,11 +1,14 @@
 import cobra
 import sys
+import os
 
-toycon = cobra.io.read_sbml_model("/home/mstolarczyk/Uczelnia/UVA/shinyapp/dev/data/toycon.xml")
+path_in = os.path.abspath("data/toycon.xml")
+toycon = cobra.io.read_sbml_model(path_in)
 lower = int(-100)
 upper = int(100)
 toycon.reactions.get_by_id("E2").bounds = (lower, upper)
 lower = int(-100)
 upper = int(100)
 toycon.reactions.get_by_id("E4").bounds = (lower, upper)
-cobra.io.write_sbml_model(toycon,"/home/mstolarczyk/Uczelnia/UVA/shinyapp/dev/data/toycon_media2.xml")
+path_out = os.path.abspath("data/toycon_media2.xml")
+cobra.io.write_sbml_model(toycon, path_out)
--- scripts/prepare_media3.py
+++ scripts/prepare_media3.py
@@ -1,11 +1,14 @@
 import cobra
 import sys
+import os
 
-toycon = cobra.io.read_sbml_model("/home/mstolarczyk/Uczelnia/UVA/shinyapp/dev/data/toycon.xml")
+path_in = os.path.abspath("data/toycon.xml")
+toycon = cobra.io.read_sbml_model(path_in)
 lower = int(0)
 upper = int(100)
 toycon.reactions.get_by_id("E1").bounds = (lower, upper)
 lower = int(-100)
 upper = int(0)
 toycon.reactions.get_by_id("E5").bounds = (lower, upper)
-cobra.io.write_sbml_model(toycon,"/home/mstolarczyk/Uczelnia/UVA/shinyapp/dev/data/toycon_media3.xml")
+path_out = os.path.abspath("data/toycon_media3.xml")
+cobra.io.write_sbml_model(toycon, path_out)
