798e03fae5403e9d29596778bb15b4822e3ce76b
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-01-23 12:29:46 -0500

    Added dashed lines for 0 flux visualization. Corrected wrong edges sizes when resetting KOs

--- server.R
+++ server.R
@@ -176,7 +176,8 @@ shinyServer(function(input, output, session) {
           for (i in seq(1, length(net$mel))) {
             weights_edges = append(weights_edges, net$mel[[i]][[3]][[2]])
           }
-          visdata$edges$width = log(abs(weights_edges))
+          edgesize = log(abs(weights_edges)) + 1
+          visdata$edges$width = edgesize
           visdata$edges$title = paste("Stoichiometric coefficient: ",ceiling(weights_edges))
         }
       }
@@ -355,6 +356,9 @@ shinyServer(function(input, output, session) {
       for (i in seq(1, length(net$mel))) {
         weights_edges = append(weights_edges, net$mel[[i]][[3]][[2]])
       }
+      dashed = rep(FALSE, length(weights_edges))
+      dashed[which(weights_edges == 0)] = TRUE 
+      visdata$edges$dashes = dashed
       edgesize = log(abs(weights_edges)) + 1
       visdata$edges$width = edgesize
       visdata$edges$length = 150
@@ -489,6 +493,9 @@ shinyServer(function(input, output, session) {
       for (i in seq(1, length(net$mel))) {
         weights_edges = append(weights_edges, net$mel[[i]][[3]][[2]])
       }
+      dashed = rep(FALSE, length(weights_edges))
+      dashed[which(weights_edges == 0)] = TRUE 
+      visdata$edges$dashes = dashed
       edgesize = log(abs(weights_edges)) + 1
       visdata$edges$width = edgesize
       visdata$edges$length = 150
@@ -623,6 +630,9 @@ shinyServer(function(input, output, session) {
       for (i in seq(1, length(net$mel))) {
         weights_edges = append(weights_edges, net$mel[[i]][[3]][[2]])
       }
+      dashed = rep(FALSE, length(weights_edges))
+      dashed[which(weights_edges == 0)] = TRUE 
+      visdata$edges$dashes = dashed
       edgesize = log(abs(weights_edges)) + 1
       visdata$edges$width = edgesize
       visdata$edges$length = 150
@@ -764,6 +774,9 @@ shinyServer(function(input, output, session) {
         for (i in seq(1, length(net$mel))) {
           weights_edges = append(weights_edges, net$mel[[i]][[3]][[2]])
         }
+        dashed = rep(FALSE, length(weights_edges))
+        dashed[which(weights_edges == 0)] = TRUE 
+        visdata$edges$dashes = dashed
         edgesize = log(abs(weights_edges)) + 1
         visdata$edges$width = edgesize
         visdata$edges$length = 150
@@ -933,11 +946,23 @@ shinyServer(function(input, output, session) {
       python.load(paste(working_dir,path,sep = ""))
       flux = python.get(var.name = "flux")
       path_removed = python.get(var.name = "path_removed")
+      path_ko = path_removed
       fluxes = python.get(var.name = "fluxes")
       fluxes_output = t(rbind(t(names(fluxes)), t(fluxes)))
       fluxes_output[, 1] = paste("R_", fluxes_output[, 1], sep = "")
       rownames(fluxes_output) = c()
       colnames(fluxes_output) = c("Reaction", "Flux")
+      
+      data = rsbml_graph((sbml_model))
+      ndata = names(data@edgeData)
+      for (i in seq(1, dim(fluxes_output)[1])) {
+        hits = which(grepl(fluxes_output[i, 1], ndata))
+        for (j in hits) {
+          data@edgeData@data[[j]]$weight = as.numeric(fluxes_output[i, 2])
+        }
+      }
+      
+      
       for (i in seq(1, dim(names_dict)[2], by = 1)) {
         #Mapping nodes IDs to names for table displaying purposes
         if (any(which(fluxes_output[, 1] == names_dict[2, i])))
@@ -948,90 +973,75 @@ shinyServer(function(input, output, session) {
       }, width = "250", caption = "Fluxes without any KOs",
       caption.placement = getOption("xtable.caption.placement", "top"),
       caption.width = getOption("xtable.caption.width", NULL))
-      color_reaction = "lightblue"
-      color_metabolite = "tomato"
       net %v% "type" = ifelse(grepl("R", names), "Reaction", "Metabolite")
       edges_names = names
-      path_ko = path_removed
-      output$text_flux_ko = renderText({
-        paste(
-          "<br/>",
-          "<br/>",
-          "<b>Objective value: ",
-          as.character(flux),
-          "</b>",
-          "<br/>",
-          "<br/>"
-        )
+      output$text_flux_media = renderText({
+        paste("<br/>",
+              "<b>Objective value: ",
+              as.character(flux),
+              "</b>",
+              "<br/>")
       })
       
-      sbml_model_ko = rsbml_read(path_ko)
-      data_ko = rsbml_graph((sbml_model_ko))
-      ndata = names(data_ko@edgeData)
-      for (i in seq(1, dim(fluxes_output)[1])) {
-        hits = which(grepl(fluxes_output[i, 1], ndata))
-        for (j in hits) {
-          data_ko@edgeData@data[[j]]$weight = fluxes_output[i, 2]
-        }
-      }
-      
-      toycon_graph = igraph.from.graphNEL(data_ko)
+      toycon_graph = igraph.from.graphNEL(data)
       net = asNetwork(toycon_graph)
       net %v% "type" = ifelse(grepl("R", names), "Reaction", "Metabolite")
       reactions_names = as.vector(unlist(net$val)[which(names(unlist(net$val)) ==
                                                           "vertex.names")][which(grepl("^R", unlist(net$val)[which(names(unlist(net$val)) ==
                                                                                                                      "vertex.names")]))])
       
-      toycon_graph_ko = igraph.from.graphNEL(data_ko)
-      visdata_ko <- toVisNetworkData(toycon_graph_ko)
-      visdata_ko$nodes$group = rep("Metabolite", length(visdata_ko$nodes$id))
-      visdata_ko$nodes$group[which(grepl("R", visdata_ko$nodes$id))] = "Reaction"
+      toycon_graph = igraph.from.graphNEL(data)
+      visdata <- toVisNetworkData(toycon_graph)
+      visdata$nodes$group = rep("Metabolite", length(visdata$nodes$id))
+      visdata$nodes$group[which(grepl("R", visdata$nodes$id))] = "Reaction"
       weights_edges = c()
       for (i in seq(1, length(net$mel))) {
         weights_edges = append(weights_edges, net$mel[[i]][[3]][[2]])
       }
+      dashed = rep(FALSE, length(weights_edges))
+      dashed[which(weights_edges == 0)] = TRUE 
+      visdata$edges$dashes = dashed
       edgesize = log(abs(weights_edges)) + 1
-      visdata_ko$edges$width = edgesize
-      visdata_ko$edges$length = 150
-      visdata_ko$edges$title = paste("Flux: ",ceiling(weights_edges))
-      net_ko = asNetwork(toycon_graph_ko)
-      names_ko = unlist(net_ko$val)[seq(2, length(unlist(net_ko$val)), 2)]
+      visdata$edges$width = edgesize
+      visdata$edges$length = 150
+      visdata$edges$title = paste("Flux: ",ceiling(weights_edges))
+      #visdata$edges$arrows = c("from", "to")
+      net = asNetwork(toycon_graph)
+      names = unlist(net$val)[seq(2, length(unlist(net$val)), 2)]
       
       #Setting colors according to node class
-      color_reaction_ko = "lightblue"
-      color_metabolite_ko = "tomato"
-      net_ko %v% "type" = ifelse(grepl("R", names_ko), "Reaction", "Metabolite")
-      edges_names_ko = names_ko
+      color_reaction = "lightblue"
+      color_metabolite = "tomato"
+      net %v% "type" = ifelse(grepl("R", names), "Reaction", "Metabolite")
+      edges_names = names
       #Setting names
-      for (i in seq(1, length(names_ko))) {
-        if (any(names(sbml_model_ko@model@species) == as.character(names_ko[i]))) {
-          metabolite_name_ko = sbml_model_ko@model@species[[which(names(sbml_model@model@species) == as.character(names_ko[i]))]]@name
-          compartment_ko = sbml_model_ko@model@species[[which(names(sbml_model_ko@model@species) == as.character(names_ko[i]))]]@compartment
-          metabolite_ko = paste(metabolite_name_ko, compartment_ko, sep = "_")
-          edges_names_ko[i] = metabolite_ko
+      for (i in seq(1, length(names))) {
+        if (any(names(sbml_model@model@species) == as.character(names[i]))) {
+          metabolite_name = sbml_model@model@species[[which(names(sbml_model@model@species) == as.character(names[i]))]]@name
+          compartment = sbml_model@model@species[[which(names(sbml_model@model@species) == as.character(names[i]))]]@compartment
+          metabolite = paste(metabolite_name, compartment, sep = "_")
+          edges_names[i] = metabolite
         }
         else{
-          if (any(names(sbml_model_ko@model@reactions) == as.character(names_ko[i]))) {
-            reaction_name_ko = sbml_model_ko@model@reactions[[which(names(sbml_model_ko@model@reactions) == as.character(names_ko[i]))]]@name
-            edges_names_ko[i] = reaction_name_ko
+          if (any(names(sbml_model@model@reactions) == as.character(names[i]))) {
+            reaction_name = sbml_model@model@reactions[[which(names(sbml_model@model@reactions) == as.character(names[i]))]]@name
+            edges_names[i] = reaction_name
           }
           else{
-            edges_names_ko[i] = "NoName"
+            edges_names[i] = "NoName"
           }
         }
       }
-      edges_names_ko = sapply(edges_names_ko, function(x)
-        fill_blank(x, max(nchar(
-          edges_names_ko
-        ))))
-      names_dict_ko = rbind(edges_names_ko, names_ko) #Names and IDs dictionary
-      visdata_ko$nodes$label = as.vector(edges_names_ko)
+      edges_names = sapply(edges_names, function(x)
+        fill_blank(x, max(nchar(edges_names))))
+      names_dict = rbind(edges_names, names) #Names and IDs dictionary
+      visdata$nodes$label = as.vector(edges_names)
       path="data/textbooky_coords.csv"
       if( .Platform$OS.type == "windows" ){
         path = gsub("\\\\", "/", path)
       }
       coords = read.csv(path)
-      set1 = rownames(visdata_ko$nodes)
+      set1 = rownames(visdata$nodes)
       set2 = rownames(coords)
       deleted_rxn = setdiff(set2, set1)
       if (length(deleted_rxn) > 0) {
@@ -1040,24 +1050,24 @@ shinyServer(function(input, output, session) {
       else{
         coords_deleted_rxn = coords
       }
-      visdata_ko$nodes = cbind(visdata_ko$nodes, coords_deleted_rxn)
-      visdata_ko$nodes[which(grepl("glycolysis", names_dict_ko[1, ])), "font"] = "25px arial"
-      visdata_ko$nodes[which(grepl("respiration", names_dict_ko[1, ])), "font"] = "25px arial"
-      visdata_ko$nodes[which(grepl("synthase", names_dict_ko[1, ])), "font"] = "25px arial"
-      visdata_ko$nodes[which(grepl("demand", names_dict_ko[1, ])), "font"] = "25px arial"
+      visdata$nodes = cbind(visdata$nodes, coords_deleted_rxn)
+      visdata$nodes[which(grepl("glycolysis", names_dict[1, ])), "font"] = "25px arial"
+      visdata$nodes[which(grepl("respiration", names_dict[1, ])), "font"] = "25px arial"
+      visdata$nodes[which(grepl("synthase", names_dict[1, ])), "font"] = "25px arial"
+      visdata$nodes[which(grepl("demand", names_dict[1, ])), "font"] = "25px arial"
       output$graph_ko = renderVisNetwork({
         #Plotting graph
-        visNetwork(nodes = visdata_ko$nodes, edges = visdata_ko$edges) %>%
+        visNetwork(nodes = visdata$nodes, edges = visdata$edges) %>%
           visLegend(stepX = 75,
                     stepY = 100,
                     width = 0.1) %>%
           visOptions(highlightNearest = TRUE) %>%
           visEdges(color = "black", arrows = "to") %>%
           visGroups(groupname = "Metabolite",
-                    color = color_metabolite_ko,
+                    color = color_metabolite,
                     shape = "circle") %>%
           visGroups(groupname = "Reaction",
-                    color = color_reaction_ko,
+                    color = color_reaction,
                     shape = "box") %>%
           visPhysics(barnesHut = list(
             springLength = 200,
@@ -1067,6 +1077,7 @@ shinyServer(function(input, output, session) {
           visLayout(randomSeed = 1)
       })
     })
+
     
     # APPLY KO ----------------------------------------------------------------
     observeEvent(input$apply_ko, {
@@ -1145,6 +1156,9 @@ shinyServer(function(input, output, session) {
       for (i in seq(1, length(net_ko$mel))) {
         weights_edges = append(weights_edges, net_ko$mel[[i]][[3]][[2]])
       }
+      dashed = rep(FALSE, length(weights_edges))
+      dashed[which(weights_edges == 0)] = TRUE 
+      visdata_ko$edges$dashes = dashed
       edgesize = log(abs(weights_edges)) + 1
       visdata_ko$edges$width = edgesize
       visdata_ko$edges$title = paste("Flux: ",ceiling(weights_edges))
