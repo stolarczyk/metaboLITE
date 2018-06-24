b1057108edf939e160f7f180bc907fb1396f3aa4
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-06-23 22:48:18 -0400

    fixing merge

--- README.md
+++ README.md
@@ -25,47 +25,93 @@ The *ToyCon visualization app* is an R shiny app that aids the basic understandi
 # Installation - Docker container<a name="Installation"></a>
 The most reliable, safest and **easiest installation approach** is to use the [Docker](https://www.docker.com/what-docker) platform due to its intrisic characterisitics. In order to do it user needs to run the pre-built Docker image (provided in the official Docker [repository](https://hub.docker.com/r/mstolarczyk/toyconapp/)).
 
-**1. Install the Docker CE (community edition) software on your machine**
+#### 1. Install the Docker CE (community edition) software on your machine
+
 The installation instructions can be found on the [Docker website](https://docs.docker.com/install/):
 
 * Linux
     * [Debian](https://docs.docker.com/install/linux/docker-ce/debian/)
     * [Fedora](https://docs.docker.com/install/linux/docker-ce/fedora/)
     * [Ubuntu](https://docs.docker.com/install/linux/docker-ce/ubuntu/)
-* [Windows](https://docs.docker.com/docker-for-windows/install/)
+* Windows
+    * [Windows 10 Pro/Enterprise/Education](https://docs.docker.com/docker-for-windows/install/)
+    * [Older Windows](https://docs.docker.com/toolbox/toolbox_install_windows/)
 * [MacOS](https://docs.docker.com/docker-for-mac/install/)
 
-**2. Open the Docker application to ensure it downloaded correctly**
+#### 2. Open the Docker application to ensure it installed correctly
 
-**3. Open the command window (terminal) and download the docker image from the repository** with a following [command](https://docs.docker.com/engine/reference/commandline/pull):
+- On MacOS the green dot and "Docker is running" status should be displayed in the menu after clicking the Docker icon
+
+<br/>
+<img src="./docs_pics/icon_running.png" width="20%" > 
+<figcaption>**Image 1.** LEFT: MacOS - the dot shows the Docker software status. RIGHT: Windows - ...</figcaption>
+<br/>
+
+
+#### 3. Downloading and launching the application
+
+Generally, running the commands presented in **3.2** will download and launch the application for every operating system. However, in case of MacOS and Windows, there is an option to use a graphical interface to avoid working in terminal, for details see below.
+
+**3.1 Kitematic interface installation (Windows and MacOS only)**
+
+There is a simple, yet powerful graphical user (GUI) interface available for Windows and MacOS - [Kitematic](https://kitematic.com/). Kitematic’s one click install gets Docker running on your computer and lets you control your app containers from a GUI.
+Kitematic is provided together with the Docker for Windows and should be ready to use on this stage of installation. 
+On MacOS to get Kitematic just expand the Docker menu when it is running, select Kitematic and you will be presented with the instructions on how to install it.
+
+<br/>
+<img src="./docs_pics/icon_kitematic.png" width="25%" class="center">
+<figcaption>**Image 2.** Kitematic can be launched/installed from the Docker manu.</figcaption>
+<br/>
+
+
+_In case of both operating systems_: once the Kitematic is installed, you can search for any Docker image deposited to the Docker Hub. To download the shinyapp, just search the shinyapp phrase and "create" the container deposited by the user: mstolarczyk. 
+
+<br/>
+<img src="./docs_pics/search_image_kitematic.png" width="70%" class="center">
+<figcaption>**Image 3.** Any Docker image deposited to the Docker Hub can be downloaded with Kitematic.</figcaption>
+<br/>
+
+Once the download process is completed, the container will run automatically and you can launch the application by clicking on the web preview section in Kitematic, as shown below:
+
+<br/>
+<img src="./docs_pics/launch_app_kitematic.png" width="70%" class="center">
+<figcaption>**Image 4.** The application can be launched by clicking on the web preview in the Kitematic software.</figcaption>
+<br/>
+
+**3.2 Command line Docker image management (Linux only)**
+
+Depending on your Docker installation, the superuser privilages may be required, which can be acquired with `sudo`.
+
+Open the command window (terminal) and download the docker image from the repository with a following [command](https://docs.docker.com/engine/reference/commandline/pull):
 
 ```docker pull [OPTIONS]```
 
 e.g copy and paste:
 
-``` sudo docker pull mstolarczyk/shinyapp:latest ``` (this may take some time depending on your connection)
+``` (sudo) docker pull mstolarczyk/shinyapp:latest ``` (this may take some time depending on your connection)
 
-**4. Check the image status**:
+**3.2.1. Check the image status**:
 
-``` sudo docker images```
+``` (sudo) docker images```
 
 The output of the command above should resemble the following:
 
 ``` 
 REPOSITORY              TAG                 IMAGE ID            CREATED             SIZE 
-mstolarczyk/toyconapp   newest              45f03ae5cff4        30 minutes ago      2.35GB 
+mstolarczyk/toyconapp   latest              45f03ae5cff4        30 minutes ago      2.35GB 
 ``` 
 
-**5. Run the application** using the ``` IMAGE ID ``` with a following [command](https://docs.docker.com/engine/reference/commandline/run/):
+**3.2.2. Run the application** using the ``` IMAGE ID ``` with a following [command](https://docs.docker.com/engine/reference/commandline/run/):
     
 ```docker run [OPTIONS] IMAGE [COMMAND] [ARG...] ```
 
 e.g copy and paste
 
-```sudo docker run -p 8080:8080 45f03ae5cff4 ``` (note that the ``` IMAGE ID ``` will be different in your case)
+```(sudo) docker run -p 8080:8080 45f03ae5cff4 ``` (note that the ``` IMAGE ID ``` will be different in your case)
 
 The command above will allow you to connect to the application from your web browser.
 
-**6. Copy and paste this address into your web browser**
+**3.2.3. Copy and paste this address into your web browser**
 
 ``` localhost:8080 ```
+
--- ui.R
+++ ui.R
@@ -1,4 +1,6 @@
 
+
+
 # This is the user-interface definition of a Shiny web application.
 
 library(shiny)
@@ -11,44 +13,129 @@ library(htmlwidgets)
 
 shinyUI(
   navbarPage(
-    "iNRG Model",
+    "iNRG Model", #chamnge the name
     theme = shinytheme("cosmo"),
     id = "tabs",
     fluid = T,
     collapsible = T,
     tabPanel(
       "Visualize network",
-      value="visualize",
+      value = "visualize",
       sidebarPanel(
-        verbatimTextOutput('text'),
-        fluidRow(class="myRowText",column(6,HTML("<u><b>Choose model:</b></u>")),column(2,offset = 0,popify(actionLink("choose_model_popover","",icon=icon("question-circle-o")),title = "Pick a model to run the experimets with",options = list(container = "body"),content = "There are two models available. First is a toy example which transparently shows the concepts of genome-scale metabolic modeling. The second one is a real-life example, great for investigation of such models behavior.",trigger = "click",placement = "right"))),
-        selectInput(inputId = "pick_model",label = NULL,choices = list("iNRG"="toycon","Ecoli"="ecoli"),selected = "toycon",width = "55%"),
-        fluidRow(class="myRowText",column(6,HTML("<u><b>Run experiments:</b></u>")),column(1,offset = 0,actionLink("tabs_popover","",icon=icon("question-circle-o")))),
-        fluidRow(class="myRowButton",
-                 column(7, bsButton(inputId = "change_media",block = T,label = "Change media"))
-        ),
-        fluidRow(class="myRowButton",
-                 column(7,bsButton(inputId = "ko_rxn",block = T,label = "Simulate gene knockout"))
-        ),
-        fluidRow(class="myRowButton",
-                 column(7,bsButton(inputId = "simulate_expr",block = T,label = "Integrate transcriptomic data"))
-        ),
+        h3("Experiment setup"),
+        br(),
+        br(),
+        fluidRow(class = "myRowText", column(7, HTML(
+          "<u><b>Choose the model:</b></u>"
+        )), column(
+          2,
+          offset = 0,
+          popify(
+            actionLink("choose_model_popover", "", icon = icon("question-circle")),
+            title = "Pick a model to run the experimets with",
+            options = list(container = "body"),
+            content = "There are two models available. First is a toy example which transparently shows the concepts of genome-scale metabolic modeling. The second one is a real-life example, great for investigation of such models behavior.",
+            trigger = "click",
+            placement = "right"
+          )
+        )),
+        fluidRow(class = "myRowText", column(
+          8,
+          HTML(
+            "<font color='#808080'>The choice will be effective for each experiment in this application.</font>"
+          )
+        )),
+        br(),
+        fluidRow(column(
+          7,
+          selectInput(
+            inputId = "pick_model",
+            label = NULL,
+            choices = list("iNRG" = "toycon", "Ecoli" = "ecoli"),
+            selected = "toycon",
+            width = "100%"
+          )
+        ), column(
+          2,
+          offset = 0,
+          actionLink("model_stats", "", icon = icon("info-circle"))
+        )),
+        tags$div(id = 'placeholder'),
+        fluidRow(class = "myRowText", column(7, HTML(
+          "<u><b>Select the biological objective:</b></u>"
+        )), column(
+          2,
+          offset = 0,
+          popify(
+            actionLink("select_objective_popover", "", icon = icon("question-circle")),
+            options = list(container = "body"),
+            title = "Select the biological objective",
+            content = "The biological objective is the reaction that is relevant to the problem being studied. In the case of predicting growth, the objective is biomass production. Whereas in case of investigation energy metabolism - the ATP production.",
+            trigger = "click",
+            placement = "right"
+          )
+        )),
+        tags$div(id = 'placeholder1'),
+        fluidRow(class = "myRowText", column(
+          7, HTML("<u><b>Run experiments:</b></u>")
+        ), column(
+          1, offset = 0, actionLink("tabs_popover", "", icon = icon("question-circle"))
+        )),
+        fluidRow(class = "myRowButton",
+                 column(
+                   7,
+                   bsButton(
+                     inputId = "change_media",
+                     block = T,
+                     label = "Change media"
+                   )
+                 )),
+        fluidRow(class = "myRowButton",
+                 column(
+                   7,
+                   bsButton(
+                     inputId = "ko_rxn",
+                     block = T,
+                     label = "Simulate gene knockout"
+                   )
+                 )),
+        fluidRow(class = "myRowButton",
+                 column(
+                   7,
+                   bsButton(
+                     inputId = "simulate_expr",
+                     block = T,
+                     label = "Integrate transcriptomic data"
+                   )
+                 )),
         hr(),
-        fluidRow(class="myRowText",column(10,HTML("<u><b>Visualize the metabolic network: </b></u>"))),
-        tags$head(tags$style(".myRowButton{height:50px;} .myRowText{height:50px} .myRowList{height:100px;} .myRowSmall{height:20px;}")),
-        tags$head(tags$style(HTML("hr {border-top: 2px solid #bcbcbc;}"))),
+        fluidRow(class = "myRowText", column(
+          10, HTML("<u><b>Visualize the metabolic network: </b></u>")
+        )),
+        tags$head(
+          tags$style(
+            ".myRowButton{height:50px;} .myRowText{height:50px} .myRowList{height:100px;} .myRowSmall{height:20px;}"
+          ),
+          tags$style(".fa-question-circle {color:#919499}"),
+          tags$style(".fa-info-circle {color:#919499}"),
+          tags$style(".fa-table {color:#919499}"),
+          tags$style(HTML("hr {border-top: 2px solid #bcbcbc;}"))
+        ),
         radioButtons(
           inputId = "weighting",
-          label = HTML("Display weights:"),selected = "none",
-          choices = c(
-            "None" = "none",
-            "Stoichiometry" = "stoichiometry"
-          ),width = "50%"
+          label = HTML("Display weights:"),
+          selected = "none",
+          choices = c("None" = "none",
+                      "Stoichiometry" = "stoichiometry"),
+          width = "50%"
         ),
         
         popify(
-          bsButton(inputId = "update",
-                   label = "Apply",value = T),
+          bsButton(
+            inputId = "update",
+            label = "Apply",
+            value = T
+          ),
           title = "Apply the settings",
           content = "Greater flux through reactions or stoichiometry in the model will be shown as thicker edges of the graph",
           placement = "right",
@@ -56,24 +143,41 @@ shinyUI(
         ),
         br(),
         hr(),
-        fluidRow(class="myRowText",column(5,HTML("<b>Objective value: </b>")),column(1,offset = 0,htmlOutput("text_flux")),column(1,offset = 1,popify(actionLink("flux_popover","",icon=icon("question-circle-o")),title = "Objective value",
-                                                                                                                                                      content = "Since iNRG represents the energy metabolism its biological objective is ATP production. Consequently, this reaction is maximized while solving the FBA problem. Therefore, it can be interpreted as arbitrary units of ATP.",
-                                                                                                                                                      placement = "right",
-                                                                                                                                                      trigger = "click",                                                                                                                                  options = list(container = "body")))),
+        br(),
+        h3("Results"),
+        br(),
+        br(),
+        fluidRow(
+          class = "myRowText",
+          column(5, HTML("<b>Objective value: </b>")),
+          column(1, offset = 0, htmlOutput("text_flux")),
+          column(
+            1,
+            offset = 1,
+            popify(
+              actionLink("flux_popover", "", icon = icon("question-circle")),
+              title = "Objective value",
+              content = "It represents flux through the reaction that is a biological objective of the model.",
+              placement = "right",
+              trigger = "click",
+              options = list(container = "body")
+            )
+          )
+        ),
         DT::dataTableOutput('fluxes'),
+        bsModal(
+          id = "modal_model_stats",
+          title = "Model statistics",
+          trigger = "model_stats",
+          size = "small",
+          DT::dataTableOutput("model_stats")
+        ),
         width = 4
       ),
-      mainPanel(visNetworkOutput(
-        "graph",height = "700"
-      ), width = 8)
+      mainPanel(visNetworkOutput("graph", height = "1200"), width = 8)
     ),
-    
-    tabPanel(
-      "Help",
-      value = "help",
-      mainPanel(
-        includeMarkdown(path = "help.md")
-      )
-    )
+    tabPanel("Help",
+             value = "help",
+             mainPanel(includeMarkdown(path = "help.md")))
   )
 )
